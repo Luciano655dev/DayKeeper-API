0f922126e0515e8da9b50c5d1d02511fe5f1c6c7
Luciano655dev <lucianomenezes655@gmail.com>
2025-06-30 00:23:51 -0300

    webhook changes

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const router = express.Router()
+const axios = require("axios")
+const https = require("https")
 const Media = require("../api/models/Media")
 const Post = require("../api/models/Post")
 const AWS = require("aws-sdk")
@@ -12,63 +14,76 @@ router.post(
   "/rekognition",
   bodyParser.text({ type: "*/*" }),
   async (req, res) => {
-    console.log("REKOGNITION WEBHOOK TRIGGERED ----------")
-    const type = req.headers["x-amz-sns-message-type"]
-
-    let body
     try {
-      body = JSON.parse(req.body)
-    } catch (err) {
-      console.error("Invalid SNS message:", err)
-      return res.status(400).send("Invalid SNS message")
-    }
+      console.log("REKOGNITION WEBHOOK TRIGGERED ----------")
+      const type = req.headers["x-amz-sns-message-type"]
 
-    if (type === "SubscriptionConfirmation") {
-      console.log("Confirming subscription:", body.SubscribeURL)
-      await fetch(body.SubscribeURL)
-      return res.send("SNS confirmed")
-    }
-
-    if (type === "Notification") {
-      const msg = JSON.parse(body.Message)
-      const jobId = msg.JobId
-      const media = await Media.findOne({ jobId })
-      if (!media) {
-        console.log("Media not found")
-        return res.send("media not found")
+      let body
+      try {
+        body = JSON.parse(req.body)
+      } catch (err) {
+        console.error("Invalid SNS message:", err)
+        return res.status(400).send("Invalid SNS message")
       }
 
-      const result = await rekognition
-        .getContentModeration({ JobId: jobId })
-        .promise()
-
-      const flagged = result.ModerationLabels.some(
-        (label) =>
-          inappropriateLabels.includes(label.ModerationLabel.Name) &&
-          label.ModerationLabel.Confidence > 90
-      )
-
-      media.status = flagged ? "rejected" : "public"
-      if (flagged) console.log("THIS F MEDIA AINT SAFE")
-      await media.save()
-
-      if (!flagged && media.usedIn?.model === "Post") {
-        const allMedia = await Media.find({
-          "usedIn.refId": media.usedIn.refId,
+      if (type === "SubscriptionConfirmation") {
+        console.log("Confirming subscription:", body.SubscribeURL)
+        await axios.get(body.SubscribeURL, {
+          httpsAgent: new https.Agent({
+            rejectUnauthorized: true, // make sure it's secure
+          }),
         })
-        const allApproved = allMedia.every((m) => m.status === "public")
-
-        if (allApproved) {
-          console.log("POST IS ALL RIGHT, READY TO GO PUBLIC")
-          await Post.findByIdAndUpdate(media.usedIn.refId, { status: "public" })
-        }
+        console.log("subscription confirmed")
+        return res.send("SNS confirmed")
       }
 
-      console.log("VIDEO MODERATION FINISHED")
-      return res.send("Handled video moderation result")
-    }
+      if (type === "Notification") {
+        const msg = JSON.parse(body.Message)
+        const jobId = msg.JobId
+        const media = await Media.findOne({ jobId })
+        if (!media) {
+          console.log("Media not found")
+          return res.send("media not found")
+        }
 
-    return res.send("No-op")
+        const result = await rekognition
+          .getContentModeration({ JobId: jobId })
+          .promise()
+
+        const flagged = result.ModerationLabels.some(
+          (label) =>
+            inappropriateLabels.includes(label.ModerationLabel.Name) &&
+            label.ModerationLabel.Confidence > 90
+        )
+
+        media.status = flagged ? "rejected" : "public"
+        if (flagged) console.log("THIS F MEDIA AINT SAFE")
+        await media.save()
+
+        if (!flagged && media.usedIn?.model === "Post") {
+          const allMedia = await Media.find({
+            "usedIn.refId": media.usedIn.refId,
+          })
+          const allApproved = allMedia.every((m) => m.status === "public")
+
+          if (allApproved) {
+            console.log("POST IS ALL RIGHT, READY TO GO PUBLIC")
+            await Post.findByIdAndUpdate(media.usedIn.refId, {
+              status: "public",
+            })
+          }
+        }
+
+        console.log("VIDEO MODERATION FINISHED")
+        return res.send("Handled video moderation result")
+      }
+
+      return res.send("No-op")
+    } catch (error) {
+      console.log("Webhook Error:")
+      console.log(error)
+      return res.status(500).json("Webhook Error, please contact an admin")
+    }
   }
 )
 
